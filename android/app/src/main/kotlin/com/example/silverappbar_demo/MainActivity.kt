48a31a80aeea4cd4c97df49a6f613b87d8c8dbf9
dawirashid <77828860+dawirashid@users.noreply.github.com>
2022-02-22 12:35:43 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/silverappbar_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.silverappbar_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xadfffcfc),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            // silver app bar
+            leading: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.exit_to_app),
+            ),
+            title: const Text('SERVICE'),
+            expandedHeight: 100,
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                width: 100,
+                // color: const Color(0xffb254ea),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xffb254ea),
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                width: 100,
+                // color: const Color(0xffb254ea),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xffb254ea),
+                ),
+              ),
+            ),
+          ),SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                width: 100,
+                // color: const Color(0xffb254ea),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xffb254ea),
+                ),
+              ),
+            ),
+          ),SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                width: 100,
+                // color: const Color(0xffb254ea),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xffb254ea),
+                ),
+              ),
+            ),
+          ),SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                width: 100,
+                // color: const Color(0xffb254ea),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xffb254ea),
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                width: 100,
+                // color: const Color(0xffb254ea),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xffb254ea),
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                width: 100,
+                // color: const Color(0xffb254ea),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xffb254ea),
+                ),
+              ),
+            ),
+          ),SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                width: 100,
+                // color: const Color(0xffb254ea),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xffb254ea),
+                ),
+              ),
+            ),
+          ),SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                width: 100,
+                // color: const Color(0xffb254ea),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xffb254ea),
+                ),
+              ),
+            ),
+          ),SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                width: 100,
+                // color: const Color(0xffb254ea),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xffb254ea),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
